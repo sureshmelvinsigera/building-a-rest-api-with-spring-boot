3a3ebf1e41c9b8d6f1846e813373fc5cf91965f3
Suresh Sigera <ssigera@proton.me>
2026-04-06 12:31:45 -0400

    added ApiApplicationMainTest

---
+++ src/test/java/academy/javapro/api/ApiApplicationTest.java
@@ -0,0 +1,11 @@
+package academy.javapro.api;
+
+import org.junit.jupiter.api.Test;
+
+class ApiApplicationMainTest {
+
+    @Test
+    void mainMethodRuns() {
+        ApiApplication.main(new String[] {});
+    }
+}
