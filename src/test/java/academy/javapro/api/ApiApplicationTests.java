e144a67d5028af1d6fc5e1c724cb349fd165329d
java-pro-academy <thejavabootcamp@proton.me>
2026-04-06 10:40:29 -0400

    2.1 Setting Up a Spring Boot Project with Custom Package Structure

---
+++ src/test/java/academy/javapro/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package academy.javapro.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
